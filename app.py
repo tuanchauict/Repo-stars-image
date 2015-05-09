57fbf11f526c3c5e72d0dd465fb8a667ef899d44
Tuan Chau <tuanchauict@gmail.com>
2015-05-09 14:10:32 +0700

    remove debug

--- app.py
+++ app.py
@@ -92,4 +92,4 @@ def response_stars_image(stars):
 if __name__ == '__main__':
 	import os
 	port = int(os.environ.get("PORT", 5000))
-	app.run(debug=True, host='0.0.0.0', port=port)
+	app.run(debug=False, host='0.0.0.0', port=port)
